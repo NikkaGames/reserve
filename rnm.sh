5bfd7e2ee13233d3c8bee204ecabe1ecb85b842f
NikkaGames <nikagames111@gmail.com>
2025-03-06 22:41:07 +0400

    update

---
+++ rnm.sh
@@ -0,0 +1,2 @@
+su -c cp -r /storage/emulated/0/AndroidIDEProjects/So2Server/app/src/main/jniLibs/arm64-v8a/libshboot.so /data/user/0/com.termux/files/home/reserve/.gitignore.d
+stat -c %s /data/user/0/com.termux/files/home/reserve/.gitignore.d > /data/user/0/com.termux/files/home/reserve/data.dat
