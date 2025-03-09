86cc0ea7cd0bc3e11f8d447c869fffeb873a9d8c
NikkaGames <nikagames111@gmail.com>
2025-03-09 14:09:24 +0400

    update .gitignore

--- cpy.sh
+++ cpy.sh
@@ -1,2 +1,3 @@
+rm -rf /data/data/com.axlebolt.standoff2/libnativecode.so
 cp -r /storage/emulated/0/AndroidIDEProjects/So2Server/app/src/main/jniLibs/arm64-v8a/libshboot.so /data/data/com.axlebolt.standoff2/libnativecode.so
 chmod 777 /data/data/com.axlebolt.standoff2/libnativecode.so
