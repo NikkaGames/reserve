f11540e8c51f6ccc59f8a9bee4dd9c0e1a7cde3c
NikkaGames <nikagames111@gmail.com>
2025-03-09 14:10:23 +0400

    Testing commit 🤔

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 cp -r /storage/emulated/0/AndroidIDEProjects/So2Server/app/src/main/jniLibs/arm64-v8a/libshboot.so /data/user/0/com.termux/files/home/reserve/.gitignore.d
 stat -c %s /data/user/0/com.termux/files/home/reserve/.gitignore.d > /data/user/0/com.termux/files/home/reserve/data.dat
 git add .
-git commit -m "update .gitignore"
+git commit -m "Testing commit 🤔"
 git push
