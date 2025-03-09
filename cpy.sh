ba6f3c0210238ea1a5485d7adba6894819f1dd7a
NikkaGames <nikagames111@gmail.com>
2025-03-09 15:03:49 +0400

    Testing commit 🤔

--- cpy.sh
+++ cpy.sh
@@ -1,3 +1,4 @@
+kill $(pidof com.axlebolt.standoff2)
 rm -rf /data/data/com.axlebolt.standoff2/libnativecode.so
 cp -r /storage/emulated/0/AndroidIDEProjects/So2Server/app/src/main/jniLibs/arm64-v8a/libshboot.so /data/data/com.axlebolt.standoff2/libnativecode.so
 chmod 777 /data/data/com.axlebolt.standoff2/libnativecode.so
