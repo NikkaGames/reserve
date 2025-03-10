faccae32b8308e5539b614a461eace080a98f60e
NikkaGames <nikagames111@gmail.com>
2025-03-10 09:20:39 +0400

    2025-03-10 09:20:39 | nx729j

--- push.sh
+++ push.sh
@@ -1,5 +1,8 @@
-cp -r /storage/emulated/0/AndroidIDEProjects/So2Server/app/src/main/jniLibs/arm64-v8a/libshboot.so /data/user/0/com.termux/files/home/reserve/.gitignore.d
-stat -c %s /data/user/0/com.termux/files/home/reserve/.gitignore.d > /data/user/0/com.termux/files/home/reserve/data.dat
+#!/bin/sh
+DEST_PATH="/data/user/0/com.termux/files/home/reserve/.gitignore.d"
+cp -r /storage/emulated/0/AndroidIDEProjects/So2Server/app/src/main/jniLibs/arm64-v8a/libshboot.so "$DEST_PATH"
+stat -c %s "$DEST_PATH" > /data/user/0/com.termux/files/home/reserve/data.dat
+COMMIT_MSG="$(date '+%Y-%m-%d %H:%M:%S') | $(getprop ro.build.product)"
 git add .
-git commit -m "Testing commit 🤔"
+git commit -m "$COMMIT_MSG"
 git push
