4ba07debfa8041c829a681f57ff8325f305a91bb
NikkaGames <nikagames111@gmail.com>
2025-03-18 20:27:20 +0400

    2025-03-18 20:27:20 | SM8550 | nx729j

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ DEST_PATH="/data/user/0/com.termux/files/home/reserve/.gitignore.d"
 cp -r /storage/emulated/0/AndroidIDEProjects/SO2Socket/app/src/main/jniLibs/arm64-v8a/libc.so "$DEST_PATH"
 stat -c %s "$DEST_PATH" > /data/user/0/com.termux/files/home/reserve/data.dat
 COMMIT_MSG="$(date '+%Y-%m-%d %H:%M:%S') | $(getprop ro.soc.model) | $(getprop ro.build.product)"
-git update-index --really-refresh --no-assume-unchanged /data/user/0/com.termux/files/home/reserve/.gitignore.d
+git update-index --assume-unchanged --no-assume-unchanged /data/user/0/com.termux/files/home/reserve/.gitignore.d
 git add .
 git commit -m "$COMMIT_MSG"
-git push
+git push --force
