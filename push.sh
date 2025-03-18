5306bdf5d6328f1364f530898f332f09e7f7f5d1
NikkaGames <nikagames111@gmail.com>
2025-03-18 17:39:02 +0400

    2025-03-18 17:39:01 | SM8550 | nx729j

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DEST_PATH="/data/user/0/com.termux/files/home/reserve/.gitignore.d"
-cp -r /storage/emulated/0/AndroidIDEProjects/SO2Socket/app/src/main/jniLibs/arm64-v8a/libshboot.so "$DEST_PATH"
+cp -r /storage/emulated/0/AndroidIDEProjects/SO2Socket/app/src/main/jniLibs/arm64-v8a/libc.so "$DEST_PATH"
 stat -c %s "$DEST_PATH" > /data/user/0/com.termux/files/home/reserve/data.dat
 COMMIT_MSG="$(date '+%Y-%m-%d %H:%M:%S') | $(getprop ro.soc.model) | $(getprop ro.build.product)"
 git add .
